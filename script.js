e3c26b5f1009831ce3564e308723228e40a1fc6b
elchinoga <60553267+elchinoga@users.noreply.github.com>
2025-12-28 21:31:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const updates = [
     date: "2025-01-16",
     title: "Bakugo & Deku – Monthly Pack",
     description: "Bakugo and Deku are part of this month’s pack! You know where to see it 😊",
-    image: "imagenes/G9NOMeYXEAAJXlG.jpg",
+    image: "imagenes/bakugo.jpg",
     twitter: "https://twitter.com/footparadise/status/2005032550725800188"
   },
   {
